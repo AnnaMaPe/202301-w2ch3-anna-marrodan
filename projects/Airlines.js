f8f6eb34d8a62e01ce3b192e1faa69c8274e02fd
Anna Marrodán <anna.marrodan@gmail.com>
2023-01-18 22:01:20 +0100

    Do first review to all projects

--- projects/Airlines.js
+++ projects/Airlines.js
@@ -1,218 +1,256 @@
 const flights = [
-    { id: 00, to: "New York", from: "Barcelona", cost: 700, scale: false },
-    { id: 01, to: "Los Angeles", from: "Madrid", cost: 1100, scale: true },
-    { id: 02, to: "Paris", from: "Barcelona", cost: 210, scale: false },
-    { id: 03, to: "Roma", from: "Barcelona", cost: 150, scale: false },
-    { id: 04, to: "London", from: "Madrid", cost: 200, scale: false },
-    { id: 05, to: "Madrid", from: "Barcelona", cost: 90, scale: false },
-    { id: 06, to: "Tokyo", from: "Madrid", cost: 1500, scale: true },
-    { id: 07, to: "Shangai", from: "Barcelona", cost: 800, scale: true },
-    { id: 08, to: "Sydney", from: "Barcelona", cost: 150, scale: true },
-    { id: 09, to: "Tel-Aviv", from: "Madrid", cost: 150, scale: false },
-  ];
-
+  { id: 00, to: "New York", from: "Barcelona", cost: 700, scale: false },
+  { id: 01, to: "Los Angeles", from: "Madrid", cost: 1100, scale: true },
+  { id: 02, to: "Paris", from: "Barcelona", cost: 210, scale: false },
+  { id: 03, to: "Roma", from: "Barcelona", cost: 150, scale: false },
+  { id: 04, to: "London", from: "Madrid", cost: 200, scale: false },
+  { id: 05, to: "Madrid", from: "Barcelona", cost: 90, scale: false },
+  { id: 06, to: "Tokyo", from: "Madrid", cost: 1500, scale: true },
+  { id: 07, to: "Shangai", from: "Barcelona", cost: 800, scale: true },
+  { id: 08, to: "Sydney", from: "Barcelona", cost: 150, scale: true },
+  { id: 09, to: "Tel-Aviv", from: "Madrid", cost: 150, scale: false },
+];
 
 const airlinesPro = () => {
-  function quit () {
-    const quitQuestion = prompt('Would you like to leave the site? Y/N')
+  function quit() {
+    const quitQuestion = prompt("Would you like to leave the site? Y/N");
     if (quitQuestion === null) {
-      alert ('Byeeeeee') 
-    } else if (quitQuestion.toUpperCase() === 'Y') {
-      alert ('Byeeeeee') 
-    } else if (quitQuestion.toUpperCase() === 'N') {
-      adminOrUser()
-    } else if (quitQuestion.toUpperCase() !== 'Y' || quitQuestion.toUpperCase() !== 'N') {
-      alert ('Please, answer only Y or N!')
-      quit()
+      alert("Byeeeeee");
+    } else if (quitQuestion.toUpperCase() === "Y") {
+      alert("Byeeeeee");
+    } else if (quitQuestion.toUpperCase() === "N") {
+      adminOrUser();
+    } else if (
+      quitQuestion.toUpperCase() !== "Y" ||
+      quitQuestion.toUpperCase() !== "N"
+    ) {
+      alert("Please, answer only Y or N!");
+      quit();
     }
   }
 
   const adminOrUser = () => {
-    const identityQuestion = prompt ('Please, indicate if you are ADMIN or USER')
-    if (identityQuestion === null){
-        alert ('We are sorry to see you leaving. Hasta pronto!') 
+    const identityQuestion = prompt(
+      "Please, indicate if you are ADMIN or USER"
+    );
+    if (identityQuestion === null) {
+      alert("We are sorry to see you leaving. Hasta pronto!");
     } else {
-      const upperIdentityQuestion = identityQuestion.toUpperCase()
-      while (upperIdentityQuestion !== 'ADMIN' || upperIdentityQuestion !== 'USER' ) {
-        if (upperIdentityQuestion === 'ADMIN'){
-        superAdmin()
-        break
-        } else if (upperIdentityQuestion === 'USER'){
-        superUser()
-        break
+      const upperIdentityQuestion = identityQuestion.toUpperCase();
+      while (
+        upperIdentityQuestion !== "ADMIN" ||
+        upperIdentityQuestion !== "USER"
+      ) {
+        if (upperIdentityQuestion === "ADMIN") {
+          superAdmin();
+          break;
+        } else if (upperIdentityQuestion === "USER") {
+          superUser();
+          break;
         } else {
-        alert (`Please, answer only ADMIN or USER. Cheers!`)
-        adminOrUser() 
-        break
+          alert(`Please, answer only ADMIN or USER. Cheers!`);
+          adminOrUser();
+          break;
         }
       }
     }
-  }
-  adminOrUser()
+  };
+  adminOrUser();
 
-  function superUser () {
-    const costQuestion = +prompt ('Enter the maximum amount you would like to spend: ')
-    if (costQuestion > 89){
-      alert (`Down below you will find the flights that cost ${costQuestion} euros or less: `)
-      console.log(`List of flights that cost ${costQuestion} euros or less: `)
+  function superUser() {
+    const costQuestion = +prompt(
+      "Enter the maximum amount you would like to spend: "
+    );
+    if (costQuestion > 89) {
+      alert(
+        `Down below you will find the flights that cost ${costQuestion} euros or less: `
+      );
+      console.log(`List of flights that cost ${costQuestion} euros or less: `);
       flights.map((flight) => {
         if (flight.cost <= costQuestion) {
-        console.log (`Flight with ID ${flight.id} goes from ${flight.from} to ${flight.to} has a cost of ${flight.cost} and it is a direct flight. `) 
-       }       
-      })
-    } else if (costQuestion < 90){
-      alert ('We are sorry, but none of our flights match the cost you are looking for.')
-    }      
-  
-     
-    const quitUserQuestion = prompt('Would you like to check our prices again? Y/N')
-      if (quitUserQuestion === null) {
-        quit()
-      } else if (quitUserQuestion.toUpperCase() === 'Y') {
-        superUser()
-      } else if (quitUserQuestion.toUpperCase() === 'N') {
-        adminOrUser()
-      } else if (quitUserQuestion.toUpperCase() !== 'Y' || quitUserQuestion.toUpperCase() !== 'N') {
-        alert ('Please, answer only Y or N!')
-        superUser()
-      }
-} 
-  
-
-  function superAdmin () {
-    const adminQuestion = prompt ('Would you like to add more flights or delete flights? Please answer CREATE or DELETE.')
-    if (adminQuestion === null) {
-      alert ('We are sorry to see you leaving. Hasta pronto!')
-    } else if ( adminQuestion.toUpperCase() === 'CREATE'){
-      alert ('You can introduce up to 15 new flights. Please, proceed: ')
-      createFlights()
-    } else if( adminQuestion.toUpperCase() === 'DELETE'){
-      deleteFlights()
-    } else {
-      alert ('Please, answer only CREATE or DELETE!')
-      superAdmin()
+          console.log(
+            `Flight with ID ${flight.id} goes from ${flight.from} to ${flight.to} has a cost of ${flight.cost} and it is a direct flight. `
+          );
+        }
+      });
+    } else if (costQuestion < 90) {
+      alert(
+        "We are sorry, but none of our flights match the cost you are looking for."
+      );
     }
 
-  function createFlights () {
-    let id = flights.length 
+    const quitUserQuestion = prompt(
+      "Would you like to check our prices again? Y/N"
+    );
+    if (quitUserQuestion === null) {
+      quit();
+    } else if (quitUserQuestion.toUpperCase() === "Y") {
+      superUser();
+    } else if (quitUserQuestion.toUpperCase() === "N") {
+      adminOrUser();
+    } else if (
+      quitUserQuestion.toUpperCase() !== "Y" ||
+      quitUserQuestion.toUpperCase() !== "N"
+    ) {
+      alert("Please, answer only Y or N!");
+      superUser();
+    }
+  }
+
+  function superAdmin() {
+    const adminQuestion = prompt(
+      "Would you like to add more flights or delete flights? Please answer CREATE or DELETE."
+    );
+    if (adminQuestion === null) {
+      alert("We are sorry to see you leaving. Hasta pronto!");
+    } else if (adminQuestion.toUpperCase() === "CREATE") {
+      alert("You can introduce up to 15 new flights. Please, proceed: ");
+      createFlights();
+    } else if (adminQuestion.toUpperCase() === "DELETE") {
+      deleteFlights();
+    } else {
+      alert("Please, answer only CREATE or DELETE!");
+      superAdmin();
+    }
+
+    function createFlights() {
+      let id = flights.length;
       const addFLights = () => {
         let newFlight = {
           id,
-          to: '' ,
-          from: '',
-          cost: '',
-          scale: ''
+          to: "",
+          from: "",
+          cost: "",
+          scale: "",
+        };
+        do {
+          newFlight.to = prompt(
+            "Please, to proceed introduce the city of origin:"
+          );
+        } while (
+          !isNaN(newFlight.to) ||
+          newFlight.to === " " ||
+          newFlight.to === null
+        );
+        do {
+          newFlight.from = prompt("Please, introduce the city of detination:");
+        } while (
+          !isNaN(newFlight.from) ||
+          newFlight.from === " " ||
+          newFlight.from === null
+        );
+        do
+          newFlight.cost = +prompt("Please, introduce the cost of the flight:");
+        while (
+          isNaN(newFlight.cost) ||
+          newFlight.cost === " " ||
+          newFlight.cost === null
+        );
+        {
+          newFlight.scale = Boolean(
+            confirm("Please, indicate if it has an scale (OK) or no (CANCEL):")
+          );
+          flights.push(newFlight);
         }
-        do  {newFlight.to = prompt('Please, to proceed introduce the city of origin:')}     
-        while (!isNaN(newFlight.to) || newFlight.to === " " || newFlight.to === null)
-        do {newFlight.from = prompt('Please, introduce the city of detination:')}
-        while (!isNaN(newFlight.from) || newFlight.from === " " || newFlight.from === null)
-        do (newFlight.cost = +prompt('Please, introduce the cost of the flight:'))
-        while (isNaN(newFlight.cost)|| newFlight.cost === " " || newFlight.cost === null){
-        newFlight.scale = Boolean(confirm('Please, indicate if it has an scale (OK) or no (CANCEL):'))
-        flights.push(newFlight)
-        }
-      }  
-    addFLights()
+      };
+      addFLights();
 
-     while (flights.length < 15) {
-      const anotherFlight = prompt ('Would you like to introduce another flight? Y/N');
-      if (anotherFlight.toUpperCase() === 'N' || anotherFlight === null || anotherFlight === '') {
-        alert ('Down below you can see the updated list of flights:')
-        console.log('Updated list:')
-        flights.map((flight) => {
-          if (flight.scale === true) {
-            console.log(`Flight with ID ${flight.id} goes from ${flight.from} to ${flight.to} has a cost of ${flight.cost} and it is a direct flight. `)
-          } else if (flight.scale === false){
-            console.log(`Flight with ID ${flight.id} goes from ${flight.from} to ${flight.to} has a cost of ${flight.cost} and it has a layover. `)
-          }
-          }) 
-          quit()
-          break
-      } else if (anotherFlight.toUpperCase() === 'Y'){
-          id++
-          addFLights()
-        } 
-    }
-      
-    if (flights.length > 14) {
-      alert ('You have reached the maximum.')
-      alert ('Down below you can see the updated list of flights:')
-      console.log('Updated list:')
-      flights.map((flight) => {
-              console.log (`Flight with ID ${flight.id} goes from ${flight.from} to ${flight.to} has a cost of ${flight.cost} and it is a direct flight. `)
-        }) 
-      quit()
-    }
-  }
-
-
-
-  // VM721:57 Uncaught ReferenceError: Cannot access 'createFlights' before initialization
-
-
-  function deleteFlights () {
-    let flightToDelete = prompt('Please, indicate the ID of the flight you would like to delete.')
-    const selectFlightsToDelete = () => {
-         if (flightToDelete === ''|| flightToDelete === null ) {
-          deleteFlights()
-        } else if (flightToDelete === ''|| +flightToDelete > flights.length || +flightToDelete < 0 ) {
-          alert ('The flight you introduced does not exist.')
-          deleteFlights()
-        } else {
-          for (let i = 0; i < flights.length; i++) {
-            if (flights[i].id === +flightToDelete){
-              flights.splice(i, 1);
-              };
-          }
-          alert (`Down below you have the updated list for today's flights`)
-          console.log('Updated list:')
+      while (flights.length < 15) {
+        const anotherFlight = prompt(
+          "Would you like to introduce another flight? Y/N"
+        );
+        if (
+          anotherFlight.toUpperCase() === "N" ||
+          anotherFlight === null ||
+          anotherFlight === ""
+        ) {
+          alert("Down below you can see the updated list of flights:");
+          console.log("Updated list:");
           flights.map((flight) => {
-          console.log(`Flight with ID ${flight.id} goes from ${flight.from} to ${flight.to} has a cost of ${flight.cost} and it is a direct flight. `)
-          })
+            if (flight.scale === true) {
+              console.log(
+                `Flight with ID ${flight.id} goes from ${flight.from} to ${flight.to} has a cost of ${flight.cost} and it is a direct flight. `
+              );
+            } else if (flight.scale === false) {
+              console.log(
+                `Flight with ID ${flight.id} goes from ${flight.from} to ${flight.to} has a cost of ${flight.cost} and it has a layover. `
+              );
+            }
+          });
+          quit();
+          break;
+        } else if (anotherFlight.toUpperCase() === "Y") {
+          id++;
+          addFLights();
         }
-    }
-    selectFlightsToDelete()
+      }
 
-    const repeatselectFlightsToDelete = () => {
-      let questionRepeatselectFlightsToDelete = prompt('Would you like to delete another flight? Y/N')
-        if (questionRepeatselectFlightsToDelete === null || questionRepeatselectFlightsToDelete.toUpperCase() === 'N') {
-          quit()
-        } else if (questionRepeatselectFlightsToDelete.toUpperCase() === 'Y') {
-          deleteFlights()
-        } else if (questionRepeatselectFlightsToDelete.toUpperCase() !== 'Y' || questionRepeatselectFlightsToDelete.toUpperCase() !== 'N') {
-          alert ('Please, answer only Y or N!')
-          repeatselectFlightsToDelete()
+      if (flights.length > 14) {
+        alert("You have reached the maximum.");
+        alert("Down below you can see the updated list of flights:");
+        console.log("Updated list:");
+        flights.map((flight) => {
+          console.log(
+            `Flight with ID ${flight.id} goes from ${flight.from} to ${flight.to} has a cost of ${flight.cost} and it is a direct flight. `
+          );
+        });
+        quit();
       }
     }
-    repeatselectFlightsToDelete()
 
+    function deleteFlights() {
+      let flightToDelete = prompt(
+        "Please, indicate the ID of the flight you would like to delete."
+      );
+      const selectFlightsToDelete = () => {
+        if (flightToDelete === "" || flightToDelete === null) {
+          deleteFlights();
+        } else if (
+          flightToDelete === "" ||
+          +flightToDelete > flights.length ||
+          +flightToDelete < 0
+        ) {
+          alert("The flight you introduced does not exist.");
+          deleteFlights();
+        } else {
+          for (let i = 0; i < flights.length; i++) {
+            if (flights[i].id === +flightToDelete) {
+              flights.splice(i, 1);
+            }
+          }
+          alert(`Down below you have the updated list for today's flights`);
+          console.log("Updated list:");
+          flights.map((flight) => {
+            console.log(
+              `Flight with ID ${flight.id} goes from ${flight.from} to ${flight.to} has a cost of ${flight.cost} and it is a direct flight. `
+            );
+          });
+        }
+      };
+      selectFlightsToDelete();
+
+      const repeatselectFlightsToDelete = () => {
+        let questionRepeatselectFlightsToDelete = prompt(
+          "Would you like to delete another flight? Y/N"
+        );
+        if (
+          questionRepeatselectFlightsToDelete === null ||
+          questionRepeatselectFlightsToDelete.toUpperCase() === "N"
+        ) {
+          quit();
+        } else if (questionRepeatselectFlightsToDelete.toUpperCase() === "Y") {
+          deleteFlights();
+        } else if (
+          questionRepeatselectFlightsToDelete.toUpperCase() !== "Y" ||
+          questionRepeatselectFlightsToDelete.toUpperCase() !== "N"
+        ) {
+          alert("Please, answer only Y or N!");
+          repeatselectFlightsToDelete();
+        }
+      };
+      repeatselectFlightsToDelete();
+    }
   }
-  
-  }
-  
+};
 
- /*
-  
-    }  else 
-
- const flightToDelete = +prompt('INSERT ID TO DELETE')
-
- flights.splice(flightToDelete - 1, 1)
- .filter (PERO NO MODIFICA ARRAY ORIGINAL)
- console.log('This is the list of the updated flights:')
- */
-
-}
-
-
-airlinesPro()
-
-
-
-
-
-
-
-
-
+airlinesPro();
--- projects/calculatorPro/script.js
+++ projects/calculatorPro/script.js
@@ -1,148 +1,163 @@
-const numberButtons = document.querySelectorAll(".numbers")
-const operatorButtons = document.querySelectorAll(".operators")
-const equalButton = document.querySelector(".equal")
-const clearButton = document.querySelector(".clear")
-const clearAllButton = document.querySelector(".clearall")
-const squareRootButton = document.querySelector(".squareroot")
-let superiorDisplay = document.querySelector(".displaysup")
-let inferiorDisplay = document.querySelector(".displayinf")
+const numberButtons = document.querySelectorAll(".numbers");
+const operatorButtons = document.querySelectorAll(".operators");
+const equalButton = document.querySelector(".equal");
+const clearButton = document.querySelector(".clear");
+const clearAllButton = document.querySelector(".clearall");
+const squareRootButton = document.querySelector(".squareroot");
+const superiorDisplay = document.querySelector(".displaysup");
+const inferiorDisplay = document.querySelector(".displayinf");
 
-let startOver = false
-let eliminateLastElement = false
+let startOver = false;
+let eliminateLastElement = false;
 
-numberButtons.forEach((button) => { 
-    button.addEventListener("click", (event) => {
-        let value = event.target.innerText
-        if (value == "." && ((inferiorDisplay.value).includes("00"))) {
-            return
-        } 
-        if ((inferiorDisplay.value).length > 19){
-            superiorDisplay.value = '〰️ 〰️ 〰️ '
-            inferiorDisplay.value = 'Display ERROR'
-            startOver = true
-        } else {
-        if (value == "." && ((inferiorDisplay.value).includes("."))) {
-            return
-        } 
-        if (startOver){
-            superiorDisplay.value = ''
-            inferiorDisplay.value = ''
-            startOver = false
-        } 
-        if (eliminateLastElement) {
-            superiorDisplay.value = inferiorDisplay.value
-            inferiorDisplay.value = ''
-            inferiorDisplay.value += value
-            eliminateLastElement = false
-        } else {
-           inferiorDisplay.value += value
-        }
-        }
-    })
-})
+numberButtons.forEach((button) => {
+  button.addEventListener("click", (event) => {
+    const value = event.target.innerText;
+    if (value === "." && inferiorDisplay.value.includes("00")) {
+      return;
+    }
+
+    if (inferiorDisplay.value.length > 19) {
+      superiorDisplay.value = "〰️ 〰️ 〰️ ";
+      inferiorDisplay.value = "Display ERROR";
+      startOver = true;
+    } else {
+      if (value === "." && inferiorDisplay.value.includes(".")) {
+        return;
+      }
+
+      if (startOver) {
+        superiorDisplay.value = "";
+        inferiorDisplay.value = "";
+        startOver = false;
+      }
+
+      if (eliminateLastElement) {
+        superiorDisplay.value = inferiorDisplay.value;
+        inferiorDisplay.value = "";
+        inferiorDisplay.value += value;
+        eliminateLastElement = false;
+      } else {
+        inferiorDisplay.value += value;
+      }
+    }
+  });
+});
 
 operatorButtons.forEach((button) => {
-    button.addEventListener("click", (event) => {
-        let value = event.target.innerText
-        if((inferiorDisplay.value === '' && !(value === "-")) || inferiorDisplay.value === 'Syntax ERROR'){
-            return
-        } 
-        if (eliminateLastElement && startOver){
-            superiorDisplay.value = inferiorDisplay.value
-            superiorDisplay.value = superiorDisplay.value += value 
-            inferiorDisplay.value = ''
-            eliminateLastElement = false
-            startOver = false
-            return
-        } else if (eliminateLastElement && !startOver){
-            superiorDisplay.value = inferiorDisplay.value
-            superiorDisplay.value = superiorDisplay.value += value 
-            inferiorDisplay.value = ''
-            eliminateLastElement = false
-            return
-        }
-        if (startOver){
-            superiorDisplay.value = result()
-            inferiorDisplay.value = '' 
-            superiorDisplay.value = superiorDisplay.value += value
-            startOver = false
-            return
-        } 
-        superiorDisplay.value = superiorDisplay.value += inferiorDisplay.value += value 
-        inferiorDisplay.value = ''
-        
-    })
-})
+  button.addEventListener("click", (event) => {
+    const value = event.target.innerText;
+    if (
+      (inferiorDisplay.value === "" && !(value === "-")) ||
+      inferiorDisplay.value === "Syntax ERROR"
+    ) {
+      return;
+    }
+
+    if (eliminateLastElement && startOver) {
+      superiorDisplay.value = inferiorDisplay.value;
+      superiorDisplay.value = superiorDisplay.value += value;
+      inferiorDisplay.value = "";
+      eliminateLastElement = false;
+      startOver = false;
+      return;
+    }
+
+    if (eliminateLastElement && !startOver) {
+      superiorDisplay.value = inferiorDisplay.value;
+      superiorDisplay.value = superiorDisplay.value += value;
+      inferiorDisplay.value = "";
+      eliminateLastElement = false;
+      return;
+    }
+
+    if (startOver) {
+      superiorDisplay.value = result();
+      inferiorDisplay.value = "";
+      superiorDisplay.value = superiorDisplay.value += value;
+      startOver = false;
+      return;
+    }
+
+    superiorDisplay.value =
+      superiorDisplay.value +=
+      inferiorDisplay.value +=
+        value;
+    inferiorDisplay.value = "";
+  });
+});
 
 clearAllButton.addEventListener("click", (event) => {
-    let value = event.target.innerText
-    superiorDisplay.value = ''
-    inferiorDisplay.value = ''
-    startOver = false
-})
+  const value = event.target.innerText;
+  superiorDisplay.value = "";
+  inferiorDisplay.value = "";
+  startOver = false;
+});
 
 clearButton.addEventListener("click", (event) => {
-    let value = event.target.innerText
-    if (inferiorDisplay.value == 'Syntax ERROR') {
-        superiorDisplay.value = ''
-        inferiorDisplay.value = ''
-    }
-    inferiorDisplay.value = inferiorDisplay.value.slice(0, -1)
-    eliminateLastElement = true
-})
+  const value = event.target.innerText;
+  if (inferiorDisplay.value == "Syntax ERROR") {
+    superiorDisplay.value = "";
+    inferiorDisplay.value = "";
+  }
+
+  inferiorDisplay.value = inferiorDisplay.value.slice(0, -1);
+  eliminateLastElement = true;
+});
 
 squareRootButton.addEventListener("click", (event) => {
-    let value = event.target.innerText
-    let squareRootResult = squareRoot()
-    if (!(squareRootResult)){
-        inferiorDisplay.value = 'Syntax ERROR'
-        superiorDisplay.value = '➿➿➿➿'
-        startOver = true
-    } else {
-        superiorDisplay.value = '√' + inferiorDisplay.value 
-        inferiorDisplay.value = squareRootResult
-        startOver = true
-    }
-})
+  const value = event.target.innerText;
+  const squareRootResult = squareRoot();
+  if (!squareRootResult) {
+    inferiorDisplay.value = "Syntax ERROR";
+    superiorDisplay.value = "➿➿➿➿";
+    startOver = true;
+  } else {
+    superiorDisplay.value = "√" + inferiorDisplay.value;
+    inferiorDisplay.value = squareRootResult;
+    startOver = true;
+  }
+});
 
 equalButton.addEventListener("click", (event) => {
-    if (inferiorDisplay.value == '') {
-        return
-    }
-    startOver = true
-    superiorDisplay.value = superiorDisplay.value += inferiorDisplay.value 
-    if (!result()){
-        inferiorDisplay.value = 'Syntax ERROR'
-        superiorDisplay.value = '♾️♾️♾️♾️♾️'
-    } 
-    if (eliminateLastElement){
-        inferiorDisplay.value = result()
-        superiorDisplay.value = superiorDisplay.value.slice(0, -1)
-        eliminateLastElement = true  
-    } else {
-    inferiorDisplay.value = result() 
-    }
-})
+  if (inferiorDisplay.value == "") {
+    return;
+  }
+
+  startOver = true;
+  superiorDisplay.value = superiorDisplay.value += inferiorDisplay.value;
+  if (!result()) {
+    inferiorDisplay.value = "Syntax ERROR";
+    superiorDisplay.value = "♾️♾️♾️♾️♾️";
+  }
+
+  if (eliminateLastElement) {
+    inferiorDisplay.value = result();
+    superiorDisplay.value = superiorDisplay.value.slice(0, -1);
+    eliminateLastElement = true;
+  } else {
+    inferiorDisplay.value = result();
+  }
+});
 
 const result = () => {
-    if ((superiorDisplay.value).includes('√')){
-        superiorDisplay.value = inferiorDisplay.value
-    } 
-    let checkResult = eval(superiorDisplay.value) 
-    if (checkResult === Infinity) {
-        return false
-    } else {
-        return checkResult
-    }
-}
+  if (superiorDisplay.value.includes("√")) {
+    superiorDisplay.value = inferiorDisplay.value;
+  }
 
+  const checkResult = eval(superiorDisplay.value);
+  if (checkResult === Infinity) {
+    return false;
+  }
+
+  return checkResult;
+};
 
 const squareRoot = () => {
-    let resultSquareRoot = Math.sqrt(inferiorDisplay.value)
-    if (resultSquareRoot == NaN) {
-        return false
-    } else {
-        return resultSquareRoot
-    }
-}
+  const resultSquareRoot = Math.sqrt(inferiorDisplay.value);
+  if (resultSquareRoot === NaN) {
+    return false;
+  }
 
+  return resultSquareRoot;
+};
